912392298380de09bf70eea3826e389eca8040ff
Carlos Jacod <Carlos.A.Jacod@gds.ey.com>
2020-02-20 21:48:05 -0300

    minor fixes

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+// @ts-nocheck
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const callbackRouter = require('./routes/callback');
 const topRouter = require('./routes/top');
+const User = require('./models/User');
+
 
 const app = express();
 
@@ -28,18 +32,18 @@ app.use('/top', topRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- models/User.js
+++ models/User.js
@@ -1,92 +1,58 @@
-const uuid = require("uuid/v4");
-const db = require("../lib/database");
+// @ts-nocheck
+const Sequelize = require('sequelize');
+const sequelize = require('../lib/database');
 
-/**
- * @constructor User - creates an user abstraction for the db
-
- * @param {object} user - object with the user data
- * @param {string} user.display_name - the user name
- * @param {string} user.email - the user email
- * @param {string} user.href - the user profile
- * @param {string} user.email - defines the user account type
- * @param {object} user.images - an array with the user profile images
- * @param {string} user._id - unique id
- */
-
-class User {
-  constructor({
-    display_name,
-    email,
-    href,
-    product,
-    images,
-    _id = 0,
-    refresh_token
-  }) {
-    this.display_name = display_name;
-    this.email = email;
-    this.href = href;
-    this.product = product;
-    this.images = images;
-    this._id = _id;
-  }
-
-  /**
-   * @static
-   * @method
-   * method for searching an user
-   * @param {object} user
-   * @param {object} user - object with the user data
-   * @param {string} user.display_name - the user name
-   * @param {string} user.email - the user email
-   * @param {string} user.href - the user profile
-   * @param {string} user.email - defines the user account type
-   * @param {object} user.images - an array with the user profile images
-   * @param {string} user._id - unique id
-   *
-   * @returns User
-   */
-
-  static getUser(user) {
-    const result = db
-      .get("users")
-      .find(user)
-      .value();
-
-    if (result) {
-      return new User(result);
-    } else {
-      return null;
+// @ts-ignore
+const Model = Sequelize.Model;
+class User extends Model {}
+User.init(
+    {
+        // attributes
+        FirstName: {
+            type: Sequelize.STRING
+        },
+        LastName: {
+            type: Sequelize.STRING
+        },
+        UserName: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        Mail: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        ProviderID: {
+            type: Sequelize.STRING
+        },
+        Main: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false
+        },
+        ProviderType: {
+            type: Sequelize.INTEGER
+        },
+        AuthApi: {
+            type: Sequelize.STRING
+        },
+        Created: {
+            type: Sequelize.DATE,
+            allowNull: false
+        },
+        Updated: {
+            type: Sequelize.DATE
+        },
+        LastLogin: {
+            type: Sequelize.DATE
+        }
+    },
+    {
+        sequelize,
+        modelName: 'Users'
     }
-  }
+);
 
-  /**
-   * @method
-   * Method for saving an user
-   */
-  save() {
-    const searchRef = db.get("users");
-    if (this._id) {
-      return searchRef
-        .find({
-          display_name: this.display_name
-        })
-        .assign({ ...this })
-        .write();
-    } else {
-      this._id = uuid();
-      return searchRef
-        .push({
-          display_name: this.display_name,
-          email: this.email,
-          href: this.href,
-          product: this.product,
-          images: this.images,
-          _id: this._id
-        })
-        .write();
-    }
-  }
-}
-
-module.exports = User;
+// Create a new user
+User.create({ FirstName: 'Jane', LastName: 'Doe' }).then(jane => {
+  console.log("Jane's auto-generated ID:", jane.id);
+});
